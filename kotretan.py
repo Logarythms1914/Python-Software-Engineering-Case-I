1e2db61fb74dffa8ec99b89069fd0a99b3442ce5
Logarythms1914 <parmenas52@gmail.com>
2026-02-24 15:39:48 +0800

    Memperbaiki kode pada check_plan dengan menambahkan tampilan tabel benefit plan yang sedang dipakai.

--- kotretan.py
+++ kotretan.py
@@ -13,21 +13,21 @@ class User:
         username = username.lower()
         self.username = username.capitalize()
     
+    benefit_plan = [['Services', 'Basic Plan', 'Standard Plan', 'Premium Plan'],
+                    ['can_stream:', True, True, True],
+                    ['can_download:', True, True, True],
+                    ['has_SD:', True, True, True],
+                    ['has_HD:', False, True, True],
+                    ['has_UHD:', False, False, True],
+                    ['num_of_devices:', 1, 2, 4],
+                    ['content:', '3rd party movie only', 
+                     'Basic Plan Content + Sports (F1, Footbal, Basketball)',
+                     'Basic Plan + Standard Plan + PacFlix Original Series or Movie'],
+                    ['price (Rp):', 120_000, 160_000, 200_000]
+                    ]  # tabel semua plan dan benefitnya
 
     def check_benefit(self):
-        benefit_plan = [['Services', 'Basic Plan', 'Standard Plan', 'Premium Plan'],
-                        ['can_stream:', True, True, True],
-                        ['can_download:', True, True, True],
-                        ['has_SD:', True, True, True],
-                        ['has_HD:', False, True, True],
-                        ['has_UHD:', False, False, True],
-                        ['num_of_devices:', 1, 2, 4],
-                        ['content:', '3rd party movie only', 
-                         'Basic Plan Content + Sports (F1, Footbal, Basketball)',
-                         'Basic Plan + Standard Plan + PacFlix Original Series or Movie'],
-                         ['price (Rp):', 120_000, 160_000, 200_000]
-                       ]
-        print(tabulate(benefit_plan, headers='firstrow', \
+        print(tabulate(self.benefit_plan, headers='firstrow', \
                        tablefmt='mixed_grid', maxcolwidths=[20,25,25,25]))
     
 
@@ -37,5 +37,11 @@ class User:
         else:
             self.current_plan = data[self.username][0]
             self.duration_plan = data[self.username][1]
-        print(f'Kamu sedang berlangganan {self.current_plan} dengan '
-              f'durasi {self.duration_plan} bulan')
+            print(f'Kamu sedang berlangganan {self.current_plan} dengan '
+                  f'durasi {self.duration_plan} bulan.')
+            print(f'Berikut merupakan tabel benefit {self.current_plan}')
+            benefit_current_plan = [[x[0],x[self.benefit_plan[0].index(self.current_plan)]] 
+                                    for x in self.benefit_plan
+                                   ] # tabel benefit plan yang sedang digunakan user
+            print(tabulate(benefit_current_plan,headers='firstrow',tablefmt='mixed_grid'))
+        
