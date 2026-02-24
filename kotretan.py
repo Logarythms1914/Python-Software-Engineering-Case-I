66cbed17c0725f03dceb6e546a5efe328c290fb3
Logarythms1914 <parmenas52@gmail.com>
2026-02-24 16:58:09 +0800

    Mengeluarkan benefit_plan dari class User dan otomatis mengubah semua self.benefit_plan menjadi benefit_plan. Juga membuat class baru bernama NewUser dengan atribut username dan method check_benefit().

--- kotretan.py
+++ kotretan.py
@@ -7,28 +7,28 @@ data = {
     "Bagus": ["Basic Plan", 11, "bagus-9f92"]
 }  # Database PacFlix
 
+benefit_plan = [['Services', 'Basic Plan', 'Standard Plan', 'Premium Plan'],
+                ['can_stream:', True, True, True],
+                ['can_download:', True, True, True],
+                ['has_SD:', True, True, True],
+                ['has_HD:', False, True, True],
+                ['has_UHD:', False, False, True],
+                ['num_of_devices:', 1, 2, 4],
+                ['content:', '3rd party movie only', 
+                 'Basic Plan Content + Sports (F1, Footbal, Basketball)',
+                 'Basic Plan + Standard Plan + PacFlix Original Series or Movie'],
+                ['price (Rp):', 120_000, 160_000, 200_000]
+                ]  # tabel semua plan dan benefitnya
+
 
 class User:
     def __init__(self,username):
         username = username.lower()
         self.username = username.capitalize()
-    
-    benefit_plan = [['Services', 'Basic Plan', 'Standard Plan', 'Premium Plan'],
-                    ['can_stream:', True, True, True],
-                    ['can_download:', True, True, True],
-                    ['has_SD:', True, True, True],
-                    ['has_HD:', False, True, True],
-                    ['has_UHD:', False, False, True],
-                    ['num_of_devices:', 1, 2, 4],
-                    ['content:', '3rd party movie only', 
-                     'Basic Plan Content + Sports (F1, Footbal, Basketball)',
-                     'Basic Plan + Standard Plan + PacFlix Original Series or Movie'],
-                    ['price (Rp):', 120_000, 160_000, 200_000]
-                    ]  # tabel semua plan dan benefitnya
 
 
     def check_benefit(self):
-        print(tabulate(self.benefit_plan, headers='firstrow', \
+        print(tabulate(benefit_plan, headers='firstrow', \
                        tablefmt='mixed_grid', maxcolwidths=[20,25,25,25]))
     
 
@@ -38,8 +38,8 @@ class User:
         else:
             self.current_plan = data[self.username][0]  # plan yg sdng digunakan
             self.duration_plan = data[self.username][1] # durasi plan yg sdng digunakan
-            benefit_current_plan = [[x[0],x[self.benefit_plan[0].index(self.current_plan)]] 
-                                    for x in self.benefit_plan
+            benefit_current_plan = [[x[0],x[benefit_plan[0].index(self.current_plan)]] 
+                                    for x in benefit_plan
                                    ] # tabel benefit plan yang sedang digunakan user
             
             print(f'Kamu sedang berlangganan {self.current_plan} dengan '
@@ -54,8 +54,8 @@ class User:
         else:
             self.current_plan = data[self.username][0]
             self.duration_plan = data[self.username][1]
-            index_current_plan = self.benefit_plan[0].index(self.current_plan)
-            index_new_plan = self.benefit_plan[0].index(new_plan)
+            index_current_plan = benefit_plan[0].index(self.current_plan)
+            index_new_plan = benefit_plan[0].index(new_plan)
             if index_new_plan >= index_current_plan:
                 '''
                 pernyataan 'index_new_plan >= index_current_plan' bertujuan untuk
@@ -64,18 +64,29 @@ class User:
                 dan sebaliknya.
                 '''
                 if self.duration_plan > 12:
-                    new_plan_price = self.benefit_plan[-1][index_new_plan]
+                    new_plan_price = benefit_plan[-1][index_new_plan]
                     discount = 5/100
 
                     final_price = new_plan_price - new_plan_price * discount
 
                     print(f'Total biaya langganan ke {new_plan} adalah Rp{int(final_price):_}')
                 else:
-                    final_price = self.benefit_plan[-1][index_new_plan]
+                    final_price = benefit_plan[-1][index_new_plan]
                     
                     print(f'Total biaya langganan ke {new_plan} adalah Rp{final_price:_}')
             else:
                 raise Exception('Maaf, tidak bisa berlangganan downgrade plan.')
+
+
+class NewUser:
+    def __init__(self,username):
+        username = username.lower()
+        self.username = username.capitalize()
+
+
+    def check_benefit(self):
+        print(tabulate(benefit_plan, headers='firstrow', \
+                       tablefmt='mixed_grid', maxcolwidths=[20,25,25,25]))
 
 
                 
